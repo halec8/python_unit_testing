f92d1c08a8bbddd2e7a8942f6976c9e252ce2512
Cody Hale <hale.l.c91@gmail.com>
2022-01-22 12:44:47 -0600

    Initial commit

---
+++ Operations.py
@@ -0,0 +1,25 @@
+# Cody Hale
+# Python 202202 CIS189 22250
+# Arithmetic Operations Assignment
+# 1/13/2022
+
+# variables
+addition = 5 + 10  # addition
+subtraction = 10 - 2  # subtraction
+multiplication = 15 * 20  # multiplication
+exponent = 5 ** 20  # exponent
+division = 2000 / 100  # division
+modulus = 9 % 4  # modulus
+floor = 20 // 6  # floor division
+floor2 = -20 // 6  # floor division
+
+# Equations and what the end result is
+
+print("Addition: " + "5 + 10" + " = " + str(addition))
+print("Subtraction: " + "10 - 2" + " = " + str(subtraction))
+print("Multiplication: " + "15 * 20" + " = " + str(multiplication))
+print("Exponent: " + "5 ** 20" + " = " + str(exponent))
+print("Division: " + "2000 / 100" + " = " + str(division))
+print("Modulus: " + "9 % 4" + " = " + str(modulus))
+print("Floor Division: " + "20 // 6" + " = " + str(floor))
+print("Floor Division: " + "-20 // 6" + " = " + str(floor2))
